9ecd61d7f26aa4f5c5ac297cf0348c44a02006b2
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-10 20:54:00 -0500

    Own Array in Data Structure

--- python/estruc.datosPython/array.py
+++ python/estruc.datosPython/array.py
@@ -1,19 +1,33 @@
+class Array(object):
+    "Represents an array."
 
-
-class Array:
-    def __init__(self, capacity, fill_value= None):
+    def __init__(self, capacity, fill_value = None):
+        """
+        Args:
+            capacity (int): static size of the array.
+            fill_value (any, optional): value at each position. Defaults to None.
+        """
         self.items = list()
         for i in range(capacity):
-            self.items(fill_value)
+            self.items.append(fill_value)
 
-    def __len__ (self):
+    def __len__(self):
+        """Returns capacity of the array."""
         return len(self.items)
 
     def __str__(self):
-        return str(self.items) 
+        """Returns string representation of the array"""
+        return str(self.items)
 
     def __iter__(self):
+        """Supports traversal with a for loop."""
         return iter(self.items)
-    
-    def __getitem__ (self, index, new_item):
+
+    def __getitem__(self, index):
+        """Subscrit operator for access at index."""
+        return self.items[index]
+
+    def __setitem__(self, index, new_item):
+        """Subscript operator for replacement at index."""
         self.items[index] = new_item
+        
