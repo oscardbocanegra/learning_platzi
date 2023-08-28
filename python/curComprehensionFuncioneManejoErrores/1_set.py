8459a3fb27dbba1ace745b3d47e87b508d24ec30
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-08-28 18:31:22 -0500

    conjuntos con Python

---
+++ python/curComprehensionFuncioneManejoErrores/1_set.py
@@ -0,0 +1,20 @@
+#On the following code we will se what is sets and how it works, 
+#so the way to defined a set is with this brackets, and if we have
+#any repetitive element the set is going to delete that repetitive ç
+#element on the moment to print it
+
+set_countries = {"Col", "Arg", "Bra", "Col"}
+print(set_countries)
+print(type(set_countries))
+print("\n")
+
+#Or this will hapend the same with a set of numbers
+set_numbers = {1, 2, 3, 3, 56, 76, 76}
+print(set_numbers)
+print(type(set_numbers))
+print("\n")
+
+#And it is possible as well create a set with differents types 
+set_types = {"Col", False, 1.34, 4}
+print(set_types)
+print(type(set_types))
