e24b4d6991bb4268627ca701d4b21bf27dde81b8
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-09-15 06:22:08 -0500

    docs

--- backend_python/curso_fastAPI/main.py
+++ backend_python/curso_fastAPI/main.py
@@ -1,2 +1,12 @@
 # We will import the module of fastAPI
 from fastapi import FastAPI
+
+#Now we will create an instance of FastAPI
+app = FastAPI()
+#On the following code we will change the name on the /docs
+app.title = "Mi aplicacion con FastAPI"
+
+@app.get('/', tags=['Home'])
+def message():
+    return 'Hello world'
+
