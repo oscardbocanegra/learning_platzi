e8a04265062809daab4fbafaf7b41101ace8c34f
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-09-15 12:31:53 -0500

    parameters by id

--- backend_python/curso_fastAPI/main.py
+++ backend_python/curso_fastAPI/main.py
@@ -1,5 +1,30 @@
 # We will import the module of fastAPI
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+#Now i'll create a varible that will contain a list of a movies in where each 
+#dictionary is going to have their own description 
+movie = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview":'Avatar derives from a Sanskrit word meaning "descent," and when it first appeared in English in the late 18th century, it referred to the descent of a deity to the earth—typically, the incarnation in earthly form of Vishnu or another Hindu deity.',
+        "year": "2009",
+        "rating": 7.4,
+        "category": "Action"
+    
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview":'Avatar derives from a Sanskrit word meaning "descent," and when it first appeared in English in the late 18th century, it referred to the descent of a deity to the earth—typically, the incarnation in earthly form of Vishnu or another Hindu deity.',
+        "year": "2009",
+        "rating": 7.4,
+        "category": "Action"
+    
+    }
+]
+
 
 #Now we will create an instance of FastAPI
 app = FastAPI()
@@ -8,5 +33,17 @@ app.title = "Mi aplicacion con FastAPI"
 
 @app.get('/', tags=['Home'])
 def message():
-    return 'Hello world'
+    return HTMLResponse('<h1>Hello world</h1>')
 
+@app.get('/movies', tags=['movies'])
+def movies():
+    return movie
+
+#So now I'll create another path but this time we are going to specificate 
+#that this will have a parameter, and i can do this with the "{}"
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    for item in movie:
+        if item ["id"] == id:
+            return item
+    return []
