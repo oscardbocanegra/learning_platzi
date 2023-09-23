b57222771510ba9d9826e6aa20a3c2b63db35161
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-09-22 21:41:29 -0500

    finalizacion curso

--- backend_python/curso_fastAPI/main.py
+++ backend_python/curso_fastAPI/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI, Body, Path, Query
+from fastapi import Depends, FastAPI, Body, HTTPException, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
-from typing import Optional, List
-from jwt_manager import create_token
+from typing import Any, Coroutine, Optional, List
+
+from starlette.requests import Request
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
+
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth =  await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != "admin@gmail.com":
+            return HTTPException (status_code=403, detail="Credenciales son invalidas")
 
 class User(BaseModel):
     email:str
@@ -63,7 +73,7 @@ def login(user: User):
         return JSONResponse(status_code=200, content=token)
 
 
-@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
     return JSONResponse(status_code=200, content=movies)
 
