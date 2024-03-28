259d0bb0f7f951efffce3b94ed38f6ebe1e77696
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-03-27 20:19:27 -0500

    metodo post

--- backend_python/fastAPI/main.py
+++ backend_python/fastAPI/main.py
@@ -1,9 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = 'Learning FastAPI'
 
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    }
+]
+
+
 @app.get('/', tags=['home'])
 def message():
-    return 'Hello world'
+    return HTMLResponse('<h1>Hello world!</h1>')
 
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movies(id: int):
+    for item in movies:
+        if item['id'] == id:
+            return item
+    return []
+
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: int):
+    return [ item for item in movies if movies['category'] == category]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
