e20d6649d93bf0f18fbb4aa20e6e4f393bef2da3
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-03-28 21:44:49 -0500

    consulta de datos

--- backend_python/fastAPI/main.py
+++ backend_python/fastAPI/main.py
@@ -5,9 +5,15 @@ from pydantic import BaseModel, Field
 from typing import Coroutine, Optional, List
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
+from config.database import Session, engine, Base
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 app.title = 'Learning FastAPI'
+
+
+Base.metadata.create_all(bind=engine)
 
 
 class JWTBearer(HTTPBearer):
@@ -78,15 +84,19 @@ def login(user: User):
 
 @app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
-    return JSONResponse(status_code=200, content=movies)
+    db = Session()
+    result = db.query(MovieModel).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
 @app.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movies(id: int = Path(ge=1, le=2000))-> Movie:
-    for item in movies:
-        if item['id'] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(status_code=404, content=[])
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={"message": "Not found"})
+
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
@@ -97,7 +107,10 @@ def get_movies_by_category(category: str, year: int = Query(min_length=5, max_le
 
 @app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
+    db = Session()
+    new_movie = MovieModel(**movie.model_dump())
+    db.add(new_movie)
+    db.commit()
     return JSONResponse(status_code=201 ,content={"mensaje": "the movie has been register"})
 
 
