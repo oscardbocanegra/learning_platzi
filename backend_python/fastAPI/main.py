53d70d64508dd093afc4e0b545223f2d986b73a2
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-03-28 19:13:12 -0500

    starting with auth

--- backend_python/fastAPI/main.py
+++ backend_python/fastAPI/main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token
 
 app = FastAPI()
 app.title = 'Learning FastAPI'
 
+class User(BaseModel):
+    user: str
+    password: str
 
 class Movie(BaseModel):
     id: Optional[int] = None
@@ -53,6 +57,11 @@ def message():
     return HTMLResponse('<h1>Hello world!</h1>')
 
 
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    return user
+
+
 @app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
 def get_movies() -> List[Movie]:
     return JSONResponse(status_code=200, content=movies)
@@ -63,7 +72,7 @@ def get_movies(id: int = Path(ge=1, le=2000))-> Movie:
     for item in movies:
         if item['id'] == id:
             return JSONResponse(content=item)
-    return JSONResponse(content=[])
+    return JSONResponse(status_code=404, content=[])
 
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
