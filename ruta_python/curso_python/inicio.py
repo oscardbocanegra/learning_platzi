95ba6c5ae8867b43ec5a3246182fe3d305f51b52
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-11-19 19:11:53 -0500

    bases python

---
+++ ruta_python/curso_python/inicio.py
@@ -0,0 +1,21 @@
+# Saber el tipo de dato
+name = "David"
+print(type(name))
+
+# Imprimir valor en posicion
+print()
+print("Imprimir valor en posicion")
+name = 'David Bocanegra'
+print(name[0])
+print(name[1])
+print(name[-1])
+
+# saber la longitud de algo
+print()
+print("saber la longitud de algo")
+print(len(name))
+
+# Uso de sep
+print()
+print("uso de sep")
+print("Nunca", "pares", "de", "aprender", sep=", ")
