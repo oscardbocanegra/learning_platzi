13f0cd1419ff6d2b08dab67da547d3240a3bd1b1
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-11-21 13:40:31 -0500

    library done

--- ruta_python/curso_python/poo_library.py
+++ ruta_python/curso_python/poo_library.py
@@ -45,3 +45,41 @@ class Library:
     def add_books(self, book):
         self.books.append(book)
         print(f"El libro {book.title} ha sido agregado")
+        
+    
+    def register_user(self, user):
+        self.users.append(user)
+        print(f"El usuario {user.name} ha sido registrado")
+        
+    def show_available_books(self):
+        print("Libros disponibles")
+        for book in self.add_books:
+            if book.available:
+                print(f"{book.available} por {book.author}")
+
+book1 = Book("El principito", "Antoine de Saint-Exupéry")
+book2 = Book("1984", "George Orwell")
+
+#Crear usuario
+user1 = User("Carli", "001")
+
+#Crear Biblioteca
+library = Library()
+library.add_book(book1)
+library.add_book(book2)
+library.register_user(user1)
+
+#Mostrar libros
+library.show_available_books()
+
+#Realizar prestamo
+user1.borrow_book(book1)
+
+#Mostrar libros
+library.show_available_books()
+
+#Devolver libro
+user1.return_book(book1)
+
+#Mostrar libros
+library.show_available_books()
