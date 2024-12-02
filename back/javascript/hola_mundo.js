ab8f9485c0f4f4dd659d75137ced9cae7f7734d5
ODBocanegra <davidbocanegrac.com>
2024-12-02 18:25:10 -0500

    hola mundo con js

--- back/javascript/hola_mundo.js
+++ back/javascript/hola_mundo.js
@@ -1,1 +1,32 @@
 console.log('Hola mundo, voy por mi ford mustang ;)')
+
+// Tipos de datos  
+
+// PRIMITIVOS
+let nombre = "David"
+
+// number
+let edad = 20
+
+// boolean
+let noHayValor = null
+
+// symbol
+let simboloUnico = Symbol('Unico')
+
+//
+let numeroGrande = 2n
+
+// COMPLEJOS
+
+//object
+let carro = {
+    marca: 'Porsche',
+    modelo: '911'
+}
+
+// array
+let frutas = ['Manzana', 'banano', "uvas"]
+
+// funciones
+function saludar(){}
