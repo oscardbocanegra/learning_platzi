2f5504b608ae8f6e9b6c75ebba6fdef9b19e22d5
ODBocanegra <davidbocanegrac.com>
2024-12-03 16:38:21 -0500

    condicional if

---
+++ back/javascript/condicionales.js
@@ -0,0 +1,9 @@
+let nombre = "Oscar"
+
+if (nombre === "David") {
+    console.log(`hola ${nombre}`)
+}else if (nombre === "Oscar"){
+    console.log(`hola ${nombre}`)
+}else{
+    console.log("Tu nombre no ha sido encontrado")
+}
