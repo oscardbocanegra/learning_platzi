a7b2abe0364feac43c5c82112c8835f7a71bbcfd
ODBocanegra <davidbocanegrac.com>
2024-12-04 14:22:30 -0500

    for

---
+++ back/javascript/for.js
@@ -0,0 +1,12 @@
+/* uso basico del for
+
+for (variable: condicio, incremento){
+    codigo a ejecutar
+}
+*/
+
+let list = ["coffee", "eat", "sleep", "code", "repeat"]
+
+for (let i = 0; i < list.length; i++){
+    console.log(list[i])
+}
