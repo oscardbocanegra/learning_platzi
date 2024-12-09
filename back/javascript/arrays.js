cbb01d39fc57582fbc28f2bab63be1e493adfaaa
ODBocanegra <davidbocanegrac.com>
2024-12-09 16:23:14 -0500

    arrays with methods

--- back/javascript/arrays.js
+++ back/javascript/arrays.js
@@ -22,3 +22,32 @@ console.log(frutas);
 
 const newFruits = fruits.concat(['grape', 'kiwi'])
 console.log(newFruits)
+
+// metodos que iteran sobre un array
+
+// FIlter
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const evenNumbers = number.filter(num => num % 2 === 0)
+
+console.log(number)
+console.log(evenNumbers)
+
+
+// Reduce
+
+const numberReduce = [1, 2, 3, 4, 5]
+const sum = numberReduce.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+console.log(numberReduce)
+console.log(sum)
+
+// Slice()
+
+const animals = ["Dog", "Camel", "Cat", "Fish", "Fox", "Duck"]
+
+console.log(animals.slice(2))
+console.log(animals.slice(2, 4))
+console.log(animals.slice(1, 6))
+console.log(animals.slice(-3))
+console.log(animals.slice(2, -1))
+console.log(animals.slice())
