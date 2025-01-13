90d28093d4bb254699427c860bb7c26b738575f8
ODBocanegra <davidbocanegrac.com>
2025-01-13 17:57:38 -0500

    reading files

---
+++ ruta_python/curso_python/manipulacion_archivos/read_file.py
@@ -0,0 +1,11 @@
+# Leer un archivo linea por linea
+"""with open('caperucita.txt', 'r') as file:
+    for line in file:
+        print(line.strip())"""
+
+#Leer todas las lineas en una lista
+"""with open('caperucita.txt', 'r') as file:
+    lines = file.readlines()
+    print(lines)"""
+
+#Leer todo el archivo en una cadena
