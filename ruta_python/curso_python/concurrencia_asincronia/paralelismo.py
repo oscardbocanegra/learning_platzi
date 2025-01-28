14483f57ca6c803462d90a42a4cc4f4872143fa2
ODBocanegra <oscardbocanegra>
2025-01-27 20:22:42 -0500

    paralelismo is done

--- ruta_python/curso_python/concurrencia_asincronia/paralelismo.py
+++ ruta_python/curso_python/concurrencia_asincronia/paralelismo.py
@@ -3,3 +3,11 @@ import multiprocessing
 #Funcion que calcule el cuadrado de un numero
 def square(n):
     return n ** 2
+
+if __name__ == '__main__':
+    numbers = [1,2,3,4,5]
+
+    #crear un pool de procesos
+    with multiprocessing.Pool() as pool:
+        #Aplicar la funcion square a cada numero en la lista
+        result = pool.map(square, numbers)
