21ab29870593e1617e4239adf1eedf6e69a98d4e
oscardbocanegra <davidbocanegrac@gmail.com>
2025-01-28 16:28:19 -0500

    testing 2

--- ruta_python/testing_python/test.py
+++ ruta_python/testing_python/test.py
@@ -8,5 +8,14 @@ def test_calculate_total_with_empty_list():
     print("prueba 1")
     assert calculate_total([]) == 0
 
+def test_calculate_total_with_single_product():
+    products = [{
+        "name": "Notebook",
+        "price": 5
+    }]
+
+    assert calculate_total(products) == 5
+
 if __name__ == '__main__':
     test_calculate_total_with_empty_list()
+    test_calculate_total_with_single_product()
