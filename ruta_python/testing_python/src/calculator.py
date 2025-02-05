e4ebca9aedbbf3e6bbc14be1669cc174ef4c39d8
oscardbocanegra <davidbocanegrac@gmail.com>
2025-02-05 17:22:48 -0500

    doctest

--- ruta_python/testing_python/src/calculator.py
+++ ruta_python/testing_python/src/calculator.py
@@ -1,4 +1,9 @@
 def sum(a, b):
+    """
+    >>> sum(1, 2)
+    3
+    """
+
     return a + b
 
 def subtract(a, b):
@@ -8,6 +13,12 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+
+    """
+    >>> divide(4, 2)
+    Traceback (most recent call last):
+    ValueError: Cannot divide by zero
+    """
     if b == 0:
         raise ValueError('Cannot divide by zero')
     return a / b
