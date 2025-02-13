8bffd4829a9bde14e235095cce00d6c090fca5fc
oscardbocanegra <davidbocanegrac@gmail.com>
2025-02-13 16:07:20 -0500

    insercion de datos

--- ruta_python/django_project/my_first_project/my_first_app/models.py
+++ ruta_python/django_project/my_first_project/my_first_app/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class Car (models.Model):
     title = models.TextField(max_length=250)
-    
+    year = models.TextField(max_length=4, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.year}"
--- ruta_python/django_project/my_first_project/my_first_app/views.py
+++ ruta_python/django_project/my_first_project/my_first_app/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_view(request):
-    return render(request, 'index.html', {})
+    car_list = [
+        {'title': 'Audi A4'},
+        {'title': 'BMW 320i'},
+        {'title': 'Mercedes C200'}
+    ]
+    context = {
+        "car_list": car_list
+    }
+    return render(request, 'my_first_app/car_list.html', context)
