169acc57c02c0e314a67ed2cdaa3b05856619289
oscardbocanegra <davidbocanegrac@gmail.com>
2025-02-14 14:38:45 -0500

    relacionando 2 tablas

--- ruta_python/django_project/my_first_project/my_first_app/models.py
+++ ruta_python/django_project/my_first_project/my_first_app/models.py
@@ -7,3 +7,18 @@ class Car (models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.year}"
+    
+class Publisher (models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Book (models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
