c1871eaf26c2a34d4150ff45d5560735c1bfa82c
oscardbocanegra <davidbocanegrac@gmail.com>
2025-03-27 16:07:00 -0500

    gestion de vistas

--- ruta_python/djangoRest/patients/views.py
+++ ruta_python/djangoRest/patients/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from .serializers import PatientSerializer
+from .models import Patient
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# GET /api/patients => Listar
+# POST /api/patients => Crear
+# GET /api/patients/<pk>/ => Detalle
+# PUT /api/patients/<pk>/ => Modificación
+# DELETE /api/patients/<pk>/ => Borrar
+
+
+@api_view(['GET', 'POST'])
+def list_patients(request):
+    if request.method == 'GET':
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detail_patient(request, pk):
+    try:
+        patient = Patient.objects.get(id=pk)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = PatientSerializer(patient, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
