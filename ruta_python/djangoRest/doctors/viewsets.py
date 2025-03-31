e0ddb584f26351ec9deaab7bee3b4157a4012f60
oscardbocanegra <davidbocanegrac@gmail.com>
2025-03-31 16:21:21 -0500

    manejos de acciones

--- ruta_python/djangoRest/doctors/viewsets.py
+++ ruta_python/djangoRest/doctors/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from doctors.models import Doctor
 from doctors.serializers import DoctorSerializer
@@ -7,4 +9,16 @@ class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
     queryset = Doctor.objects.all()
 
+    @action(["POST"], detail=True, url_path='set-on-vacation')
+    def set_on_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = True
+        doctor.save()
+        return Response({"status": "doctor is on vacation"})
     
+    @action(["POST"], detail=True, url_path='set-off-vacation')
+    def set_off_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = False
+        doctor.save()
+        return Response({"status": "doctor is not on vacation"})
